7fa4b8b5c7e7c92c9db3d23d105bfe0637fbbdcd
kmi0817 <kmi0817@naver.com>
2023-12-03 19:53:51 +0900

    feat: .three 스크롤 시 element가 스르륵 등장하도록 구현

--- corp/script.js
+++ corp/script.js
@@ -17,7 +17,6 @@ gsap
 			trigger: '.two',
 			scrub: true,
 			pin: true,
-			pinSpacing: false,
 			start: 'top top',
 			end: '+=100%',
 		},
@@ -29,3 +28,51 @@ gsap
 		transformOrigin: 'left center',
 		ease: 'none',
 	});
+
+const animate = (e, direction = 1) => {
+	const coord = { x: 0, y: direction * 100 };
+
+	if (e.classList.contains('reveal-from-left')) {
+		coord.x = 100;
+		coord.y = 0;
+	} else if (e.classList.contains('reveal-from-right')) {
+		coord.x = -100;
+		coord.y = 0;
+	}
+
+	e.style.transform = `translate(${coord.x}px, ${coord.y}px)`;
+	e.style.opacity = '0';
+	gsap.fromTo(
+		e,
+		{ ...coord, autoAlpha: 0 },
+		{
+			duration: 1.25,
+			x: 0,
+			y: 0,
+			autoAlpha: 1,
+			ease: 'expo',
+			overwrite: 'auto',
+		}
+	);
+};
+
+const hide = (e) => {
+	gsap.set(e, { autoAlpha: 0 });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	gsap.registerPlugin(ScrollTrigger);
+
+	const reveals = gsap.utils.toArray('.reveal');
+	reveals.forEach((e) => {
+		hide(e);
+
+		ScrollTrigger.create({
+			trigger: e,
+			markers: true,
+			onEnter: () => animate(e),
+			onEnterBack: () => animate(e, -1),
+			onLeave: () => hide(e),
+		});
+	});
+});
