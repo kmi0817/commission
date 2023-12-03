c6ed00db99ecf1d0d15a5f157a65123fbe6c915f
kmi0817 <kmi0817@naver.com>
2023-12-03 20:01:20 +0900

    chore: animate, hide 함수 파일 분리

--- corp/scripts/script.js
+++ corp/scripts/script.js
@@ -1,3 +1,6 @@
+import { animate, hide } from './reveal-animation.js';
+
+// one
 gsap.timeline({
 	scrollTrigger: {
 		trigger: '.one',
@@ -11,6 +14,7 @@ gsap.timeline({
 	},
 });
 
+// two
 gsap
 	.timeline({
 		scrollTrigger: {
@@ -29,37 +33,7 @@ gsap
 		ease: 'none',
 	});
 
-const animate = (e, direction = 1) => {
-	const coord = { x: 0, y: direction * 100 };
-
-	if (e.classList.contains('reveal-from-left')) {
-		coord.x = 100;
-		coord.y = 0;
-	} else if (e.classList.contains('reveal-from-right')) {
-		coord.x = -100;
-		coord.y = 0;
-	}
-
-	e.style.transform = `translate(${coord.x}px, ${coord.y}px)`;
-	e.style.opacity = '0';
-	gsap.fromTo(
-		e,
-		{ ...coord, autoAlpha: 0 },
-		{
-			duration: 1.25,
-			x: 0,
-			y: 0,
-			autoAlpha: 1,
-			ease: 'expo',
-			overwrite: 'auto',
-		}
-	);
-};
-
-const hide = (e) => {
-	gsap.set(e, { autoAlpha: 0 });
-};
-
+// three
 document.addEventListener('DOMContentLoaded', () => {
 	gsap.registerPlugin(ScrollTrigger);
 
